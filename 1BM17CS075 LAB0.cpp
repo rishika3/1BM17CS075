ace0cdf2133615ca9871a397e22eaf688af51eaa
rishika3 <53818252+rishika3@users.noreply.github.com>
2019-08-07 12:21:27 +0530

    Add files via upload

---
+++ 1BM17CS075 LAB0.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+   
+   int n, largest;
+   int num[50];
+   cout<<"Enter number of elements you want to enter: ";
+   cin>>n;
+ 
+   for(int i = 0; i < n; i++) {
+      cout<<"Enter Element "<<(i+1)<< ": ";
+      cin>>num[i];
+   }
+   // Storing first array element in "largest" variable
+   largest = num[0];
+   for(int i = 1;i < n; i++) {
+     
+      if(largest < num[i])
+         largest = num[i];
+   } 
+   cout<<"Largest element in array is: "<<largest;
+   return 0;
+}
