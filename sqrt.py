76784de8875a1f3dc9d354e265c02250a940a5e6
rishika3 <53818252+rishika3@users.noreply.github.com>
2019-08-14 12:12:15 +0530

    Add files via upload

---
+++ sqrt.py
@@ -0,0 +1,18 @@
+def squareroot(n):
+ f,l=0,n
+ if n<0:
+  return "imaginary..!"
+ while(f<=l):
+  mid=int((f+l)/2)
+  if mid*mid==n:
+     ans=mid
+     break
+  elif mid*mid<n:
+     f=mid+1
+     ans=mid
+  elif mid*mid>n:
+     l=mid-1
+ return ans
+x=input("squareroot of:")
+print (squareroot(12))
+
