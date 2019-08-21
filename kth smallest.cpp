05a8671aa0b531c9d9505bdc520c679ec1c48ef9
rishika3 <53818252+rishika3@users.noreply.github.com>
2019-08-21 12:24:51 +0530

    Add files via upload

---
+++ kth smallest.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int ksmallest(int a[],int n, int k)
+{
+	for(int i=0;i<=k;i++)
+	{
+		int min=i;
+		for(int j=i+1;j<n;j++)
+		{
+			if(a[j]<a[min])
+				min=j;
+		}
+	swap(a[i],a[min]);	
+	}
+	return a[k-1];
+}
+int main()
+{
+	int n,k;
+	cout<<"enter n:";
+	cin>>n;
+	int a[n];
+	for (int i=0;i<n;i++)
+		cin>>a[i];
+	n= sizeof (a)/sizeof (a[0]);
+	cout<<"enter k:";
+	cin>>k;
+	cout<<"kth smallest element:"<< ksmallest(a,n,k);
+	return 0;
+}
