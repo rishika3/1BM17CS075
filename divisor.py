466ea12b07cbcf9867a0135375013929cfcab48d
rishika3 <53818252+rishika3@users.noreply.github.com>
2019-09-02 18:09:59 +0530

    Add files via upload

---
+++ divisor.py
@@ -0,0 +1,11 @@
+>>> i=1
+>>> n=int(input("enter a number:"))
+enter a number:15
+>>> while(i<n/2+1):
+	if n%i==0:
+			print(i)
+	i=i+1
+
+1
+3
+5
---
+++ password.py
@@ -0,0 +1,14 @@
+>>> import string
+>>> from random import randint
+>>> import random
+>>> str1=string.printable
+>>> print(str1)
+0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ 	
+
+>>> print(len(str1))
+100
+>>> for i in range(6):
+	print(str1[randint(0,100)],end="")
+
+	
+6 =aS
